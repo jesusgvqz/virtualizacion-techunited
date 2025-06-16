309d3e360d8092e5e6dfa2304fc5264aff7f5afa
Jesus Galvez Vazquez <111477236+jesusgvqz@users.noreply.github.com>
2025-06-16 12:54:45 -0600

    Create test-conn.py

---
+++ test-conn.py
@@ -0,0 +1,19 @@
+import pyodbc
+import os
+
+server = 'db-app-dev-server.database.windows.net'
+database = 'db_app_dev'
+username = 'virt'
+password = os.environ.get("DB_PASSWORD")  # o ponlo directo para pruebas
+driver = '{ODBC Driver 17 for SQL Server}'
+
+print("▶ Intentando conexión a la base de datos...")
+
+try:
+    conn = pyodbc.connect(
+        f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+    )
+    print("✅ Conexión exitosa a la base de datos")
+    conn.close()
+except Exception as e:
+    print("❌ Error de conexión:", e)
